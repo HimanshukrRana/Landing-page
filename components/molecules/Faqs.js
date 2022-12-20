092be63299701d1e4c01e339bc53a0a1428e0cc6
Himanshu Rana <hr.himanshu143@gmail.com>
2022-12-20 13:38:56 +0530

    fifth commit

--- components/molecules/Faqs.js
+++ components/molecules/Faqs.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function Faqs() {
-  function handleclick() {
-    console.log('handleClick')
-  }
+  const [expand, setExpand] = React.useState(false)
+  const expandclass = expand ? 'display' : 'hidden'
+  const answerclass = `${expandclass}`
 
   return (
     <div className="faqs mb-[8rem] smm:mt-[35px]">
@@ -17,43 +17,62 @@ export default function Faqs() {
             <div className="rounded-lg border-2 border-gray-100 dark:border-gray-700">
               <button
                 className="flex w-full items-center justify-between p-8"
-                onClick={handleclick}
+                onClick={() => setExpand(!expand)}
               >
-                <h1 className="font-semibold text-gray-700 dark:text-white">
+                <h1 className=" font-semibold text-gray-700 dark:text-white">
                   01. Can I Find the right information faster??
                 </h1>
-
-                <span className="close-treact-popup rounded-full bg-gray-200 text-gray-400">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M18 12H6"
-                    />
-                  </svg>
-                </span>
+                {expand ? (
+                  <span className="close-treact-popup rounded-full bg-gray-200 text-gray-400">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M18 12H6"
+                      />
+                    </svg>
+                  </span>
+                ) : (
+                  <span className="close-treact-popup rounded-full bg-blue-500 text-white">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                      />
+                    </svg>
+                  </span>
+                )}
               </button>
 
               <hr className="border-gray-200 dark:border-gray-700" />
-
-              <p className="answer p-8 text-sm text-gray-500 dark:text-gray-300">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eaque nobis,
-                fugit odit omnis fugiat deleniti animi ab maxime cum laboriosam recusandae facere
-                dolorum veniam quia pariatur obcaecati illo ducimus?
-              </p>
+              <div className={answerclass}>
+                <p className="answer  p-8 text-sm text-gray-500 dark:text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eaque nobis,
+                  fugit odit omnis fugiat deleniti animi ab maxime cum laboriosam recusandae facere
+                  dolorum veniam quia pariatur obcaecati illo ducimus?
+                </p>
+              </div>
             </div>
 
             <div className="rounded-lg border-2 border-gray-100 dark:border-gray-700">
               <button
                 className="flex w-full items-center justify-between p-8"
-                onClick={handleclick}
+                onClick={() => setExpand(!expand)}
               >
                 <h1 className="font-semibold text-gray-700 dark:text-white">
                   02. How to share feature demos and ideas?
@@ -78,17 +97,19 @@ export default function Faqs() {
               </button>
               <hr className="border-gray-200 dark:border-gray-700" />
 
-              <p className="answer hidden p-8 text-sm text-gray-500 dark:text-gray-300">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eaque nobis,
-                fugit odit omnis fugiat deleniti animi ab maxime cum laboriosam recusandae facere
-                dolorum veniam quia pariatur obcaecati illo ducimus?
-              </p>
+              <div className={answerclass}>
+                <p className="answer  p-8 text-sm text-gray-500 dark:text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eaque nobis,
+                  fugit odit omnis fugiat deleniti animi ab maxime cum laboriosam recusandae facere
+                  dolorum veniam quia pariatur obcaecati illo ducimus?
+                </p>
+              </div>
             </div>
 
             <div className="rounded-lg border-2 border-gray-100 dark:border-gray-700">
               <button
                 className="flex w-full items-center justify-between p-8"
-                onClick={handleclick}
+                onClick={() => setExpand(!expand)}
               >
                 <h1 className="font-semibold text-gray-700 dark:text-white">
                   03. Do i need a referral?
@@ -113,17 +134,19 @@ export default function Faqs() {
               </button>
               <hr className="border-gray-200 dark:border-gray-700" />
 
-              <p className="answer hidden p-8 text-sm text-gray-500 dark:text-gray-300">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eaque nobis,
-                fugit odit omnis fugiat deleniti animi ab maxime cum laboriosam recusandae facere
-                dolorum veniam quia pariatur obcaecati illo ducimus?
-              </p>
+              <div className={answerclass}>
+                <p className="answer  p-8 text-sm text-gray-500 dark:text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eaque nobis,
+                  fugit odit omnis fugiat deleniti animi ab maxime cum laboriosam recusandae facere
+                  dolorum veniam quia pariatur obcaecati illo ducimus?
+                </p>
+              </div>
             </div>
 
             <div className="rounded-lg border-2 border-gray-100 dark:border-gray-700">
               <button
                 className="flex w-full items-center justify-between p-8"
-                onClick={handleclick}
+                onClick={() => setExpand(!expand)}
               >
                 <h1 className="font-semibold text-gray-700 dark:text-white">
                   04. How to get insights from users?
@@ -148,17 +171,19 @@ export default function Faqs() {
               </button>
               <hr className="border-gray-200 dark:border-gray-700" />
 
-              <p className="answer hidden p-8 text-sm text-gray-500 dark:text-gray-300">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eaque nobis,
-                fugit odit omnis fugiat deleniti animi ab maxime cum laboriosam recusandae facere
-                dolorum veniam quia pariatur obcaecati illo ducimus?
-              </p>
+              <div className={answerclass}>
+                <p className="answer  p-8 text-sm text-gray-500 dark:text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eaque nobis,
+                  fugit odit omnis fugiat deleniti animi ab maxime cum laboriosam recusandae facere
+                  dolorum veniam quia pariatur obcaecati illo ducimus?
+                </p>
+              </div>
             </div>
 
             <div className="rounded-lg border-2 border-gray-100 dark:border-gray-700">
               <button
                 className="flex w-full items-center justify-between p-8"
-                onClick={handleclick}
+                onClick={() => setExpand(!expand)}
               >
                 <h1 className="font-semibold text-gray-700 dark:text-white">
                   05. Can I develop my website without code?
@@ -183,11 +208,13 @@ export default function Faqs() {
               </button>
               <hr className="border-gray-200 dark:border-gray-700" />
 
-              <p className=" answer hidden p-8 text-sm text-gray-500 dark:text-gray-300">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eaque nobis,
-                fugit odit omnis fugiat deleniti animi ab maxime cum laboriosam recusandae facere
-                dolorum veniam quia pariatur obcaecati illo ducimus?
-              </p>
+              <div className={answerclass}>
+                <p className="answer  p-8 text-sm text-gray-500 dark:text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas eaque nobis,
+                  fugit odit omnis fugiat deleniti animi ab maxime cum laboriosam recusandae facere
+                  dolorum veniam quia pariatur obcaecati illo ducimus?
+                </p>
+              </div>
             </div>
           </div>
         </div>
--- components/molecules/Navbar.js
+++ components/molecules/Navbar.js
@@ -6,8 +6,8 @@ export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false)
   return (
     <>
-      <nav className="mx-w-full font-mono relative mb-3 flex flex-wrap  items-center  py-3 md:hidden">
-        <div className="container  flex flex-wrap items-center justify-between ">
+      <nav className="mx-w-full font-mono relative mb-3 flex w-full  flex-wrap  items-center py-3 md:hidden">
+        <div className=" flex w-full flex-wrap items-center justify-between">
           <div className="relative flex w-full justify-between lg:static lg:block lg:w-auto lg:justify-start">
             <a href="#" className="flex items-center">
               <Image
@@ -28,58 +28,57 @@ export default function Navbar({ fixed }) {
             >
               <Icon icon="uis:bars" />
             </button>
-          </div>
-          <div
-            className={
-              ' flex w-full flex-row-reverse items-center md:block md:w-auto' +
-              (navbarOpen ? ' flex' : ' hidden')
-            }
-            id="example-navbar-danger"
-          >
-            <ul className="absolute top-8 mt-4 flex flex-col rounded-lg border border-black bg-grey p-4 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-gray-900">
-              <li>
-                <a
-                  href="#"
-                  className="block rounded py-2 pl-3 pr-4 text-black hover:bg-black hover:text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-black hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-black hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-black hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-black hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-            {/* <ul className="flex list-none flex-col lg:ml-auto lg:flex-row ">
+            <div
+              className={
+                ' flex w-full flex-row-reverse items-center md:block md:w-auto' +
+                (navbarOpen ? ' flex' : ' hidden')
+              }
+              id="example-navbar-danger"
+            >
+              <ul className="absolute top-8 mt-4 flex flex-col rounded-lg border border-black bg-grey p-4 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-gray-900">
+                <li>
+                  <a
+                    href="#"
+                    className="block rounded py-2 pl-3 pr-4 text-black hover:bg-black hover:text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700"
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-black hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-black hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  >
+                    Services
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-black hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  >
+                    Pricing
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-black hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+              {/* <ul className="flex list-none flex-col lg:ml-auto lg:flex-row ">
               <li className="nav-item">
                 <a
                   className="flex items-center px-3 py-2 text-xs font-bold uppercase leading-snug text-black hover:opacity-75"
@@ -108,6 +107,7 @@ export default function Navbar({ fixed }) {
                 </a>
               </li>
             </ul> */}
+            </div>
           </div>
         </div>
       </nav>
